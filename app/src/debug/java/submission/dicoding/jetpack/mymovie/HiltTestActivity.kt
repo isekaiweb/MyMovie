a2da47372b0ab9d2a28574c9e05428dcebee7128
isekaiweb <cobasaja271@gmail.com>
2021-05-09 16:09:24 +0700

    deleted file needed to test hilt fragment

--- app/src/debug/java/submission/dicoding/jetpack/mymovie/HiltTestActivity.kt
+++
@@ -1,7 +0,0 @@
-package submission.dicoding.jetpack.mymovie
-
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class HiltTestActivity : AppCompatActivity()
