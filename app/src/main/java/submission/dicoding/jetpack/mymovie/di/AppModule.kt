f50317cba2d181f551d07ff9c4cfe8ee824fa328
isekaiweb <cobasaja271@gmail.com>
2021-05-25 19:52:51 +0700

    finished submission 1

--- app/src/main/java/submission/dicoding/jetpack/mymovie/di/AppModule.kt
+++ app/src/main/java/submission/dicoding/jetpack/mymovie/di/AppModule.kt
@@ -2,15 +2,11 @@ package submission.dicoding.jetpack.mymovie.di
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestManager
-import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import submission.dicoding.jetpack.mymovie.R
 import submission.dicoding.jetpack.mymovie.core.data.MyMovieRepo
 import submission.dicoding.jetpack.mymovie.core.domain.usecase.MyMovieInteractor
 import submission.dicoding.jetpack.mymovie.core.domain.usecase.MyMovieUseCase
--- app/src/main/java/submission/dicoding/jetpack/mymovie/ui/MyMovieApp.kt
+++ app/src/main/java/submission/dicoding/jetpack/mymovie/ui/MyMovieApp.kt
@@ -1,23 +1,19 @@
 package submission.dicoding.jetpack.mymovie.ui
 
 import android.app.Application
-import android.content.SharedPreferences
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
 class MyMovieApp : Application() {
     private val applicationScope = CoroutineScope(Dispatchers.Default)
-
     override fun onCreate() {
         super.onCreate()
         applicationScope.launch {
             Timber.plant(Timber.DebugTree())
         }
-
     }
 }
